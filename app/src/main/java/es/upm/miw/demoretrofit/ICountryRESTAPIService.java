475ef073204a8821f0b6af9b9c99f28c98296330
walericalupm <e.aldas@alumnos.upm.es>
2019-11-09 16:32:23 +0100

    Initial Commit - walerical

---
+++ app/src/main/java/es/upm/miw/demoretrofit/ICountryRESTAPIService.java
@@ -0,0 +1,25 @@
+package es.upm.miw.demoretrofit;
+
+import java.util.List;
+
+import es.upm.miw.demoretrofit.models.Country;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+
+@SuppressWarnings("Unused")
+interface ICountryRESTAPIService {
+
+    // Request method and URL specified in the annotation
+    // Callback for the parsed response is the last parameter
+
+    // https://restcountries.eu/rest/v2/name/Spain
+    @GET("/rest/v2/name/{countryName}")
+    Call<List<Country>> getCountryByName(@Path("countryName") String countryName);
+
+    // https://restcountries.eu/rest/v2/alpha/ES
+    @GET("/rest/v2/alpha/{countryCode}")
+    Call<Country> getCountryByCode(@Path("countryCode") String countryCode);
+
+}
