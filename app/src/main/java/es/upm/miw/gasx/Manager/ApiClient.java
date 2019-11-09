004b9a13d75429665c02fad275465a61b27f6743
walericalupm <e.aldas@alumnos.upm.es>
2019-11-09 23:50:01 +0100

    POJOS added, Retrofit files added - walerical

--- app/src/main/java/es/upm/miw/gasx/Manager/ApiClient.java
+++ app/src/main/java/es/upm/miw/gasx/Manager/ApiClient.java
@@ -2,6 +2,9 @@ package es.upm.miw.gasx.Manager;
 
 import android.content.Context;
 
+import java.util.List;
+
+import es.upm.miw.gasx.Constants;
 import es.upm.miw.gasx.models.GasStation;
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -10,18 +13,19 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     Context context;
+    Retrofit retrofit;
 
-    /*public ApiClient(Context context){
+    public ApiClient(Context context){
         this.context = context;
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
+        retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.GAS_API_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
 
-    public Call<GasStation> getGasStationPriceByLocality(String location) {
-        IGasRESTAPIService service = retrofit.create(ApiService.class);
-        return service.getBatch(id);
-    }*/
+    public Call<List<GasStation>> getGasStationPriceByLocality(String location) {
+        IGasRESTAPIService service = retrofit.create(IGasRESTAPIService.class);
+        return service.getGasStationPriceByLocality(location);
+    }
 
 }
--- app/src/main/java/es/upm/miw/gasx/Manager/IGasRESTAPIService.java
+++ app/src/main/java/es/upm/miw/gasx/Manager/IGasRESTAPIService.java
@@ -3,8 +3,10 @@ package es.upm.miw.gasx.Manager;
 import java.util.List;
 
 import es.upm.miw.gasx.models.Country;
+import es.upm.miw.gasx.models.GasStation;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import static es.upm.miw.gasx.Constants.GET_STATION_PRICES_ENDPOINT;
 import static es.upm.miw.gasx.Constants.GET_STATION_PRICES_LOCATION_VARIABLE;
@@ -15,8 +17,12 @@ public interface IGasRESTAPIService {
     // Request method and URL specified in the annotation
     // Callback for the parsed response is the last parameter
 
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json"
+    })
     @GET(GET_STATION_PRICES_ENDPOINT)
-    Call<List<Country>> getGasStationPriceByLocality(
+    Call<List<GasStation>> getGasStationPriceByLocality(
             @Path(GET_STATION_PRICES_LOCATION_VARIABLE) String location);
 
 
