fd7fbbeb61a2c0535a5e1fdd4d1ce5916ce40e28
walericalupm <e.aldas@alumnos.upm.es>
2019-11-10 15:26:32 +0100

    Refactor Classes!

---
+++ app/src/main/java/es/upm/miw/gasx/manager/API/ApiManager.java
@@ -0,0 +1,62 @@
+package es.upm.miw.gasx.manager.API;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.upm.miw.gasx.models.GasStation;
+import es.upm.miw.gasx.models.GasStationAPIObject;
+import retrofit2.Call;
+
+public class ApiManager {
+
+    ApiClient apiClient = new ApiClient();
+
+
+    public List<GasStation>gegetGasStationPriceByLocality(String location){
+
+        List<GasStation>gasStations = new ArrayList();
+        Call<List<GasStationAPIObject>> getGasStations =
+                apiClient.getGasStationPriceByLocality(location);
+
+        try {
+            List<GasStationAPIObject> gasStationsRaw = getGasStations.execute().body();
+            gasStations = ConvertAPIObjetcIntoAppObject(gasStationsRaw);
+        }catch (Exception ex){}
+        return gasStations;
+
+    }
+
+    //region Converter
+
+    private List<GasStation>ConvertAPIObjetcIntoAppObject(List<GasStationAPIObject>gasStations){
+        List<GasStation>gasStationsConverted = new ArrayList();
+        for (GasStationAPIObject gasStation:gasStations) {
+
+            gasStationsConverted.add(new GasStation(
+                    gasStation.getProvincia(),
+                    gasStation.getMunicipio(),
+                    gasStation.getLocalidad(),
+                    gasStation.getDireccion(),
+                    gasStation.getLatitud(),
+                    gasStation.getLongitud(),
+                    gasStation.getPrecio_biodiesel(),
+                    gasStation.getPrecio_bioetanol(),
+                    gasStation.getPrecio_gasoleo_a(),
+                    gasStation.getPrecio_gasoleo_b(),
+                    gasStation.getPrecio_gasolina_95(),
+                    gasStation.getPrecio_gasolina_98(),
+                    gasStation.getPrecio_nuevo_gasoleo_a(),
+                    gasStation.getRotulo(),
+                    gasStation.getHorario()
+            ));
+        }
+        return gasStationsConverted;
+    }
+
+
+
+
+
+    //endregion
+
+}
---
+++ app/src/main/java/es/upm/miw/gasx/manager/API/IGasRESTAPIService.java
@@ -0,0 +1,28 @@
+package es.upm.miw.gasx.manager.API;
+
+import java.util.List;
+
+import es.upm.miw.gasx.models.GasStationAPIObject;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+import static es.upm.miw.gasx.Constants.GET_STATION_PRICES_ENDPOINT;
+import static es.upm.miw.gasx.Constants.GET_STATION_PRICES_LOCATION_VARIABLE;
+
+@SuppressWarnings("Unused")
+public interface IGasRESTAPIService {
+
+    // Request method and URL specified in the annotation
+    // Callback for the parsed response is the last parameter
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json"
+    })
+    @GET(GET_STATION_PRICES_ENDPOINT)
+    Call<List<GasStationAPIObject>> getGasStationPriceByLocality(
+            @Path(GET_STATION_PRICES_LOCATION_VARIABLE) String location);
+
+
+}
